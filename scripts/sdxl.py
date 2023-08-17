633b08cef41aae218cb265337ea578bb9cdf7ed3
Jacob Frausto <jacob_frausto@brown.edu>
2023-08-17 17:54:48 -0600

    Move model initialization and add metrics; small bug fixes

--- scripts/sdxl.py
+++ scripts/sdxl.py
@@ -1,6 +1,6 @@
 from config import API_KEY, IMAGE_DIR, ACCESS_TOKEN
-from metrics import calculate_clip_score, calculate_niqe, calculate_brisque, calculate_teng, calculate_gmsd
-from utils import generate_violinplot, generate_stripplot, generate_heatmap
+from metrics import calculate_clip_score, calculate_niqe, calculate_brisque, calculate_teng, calculate_gmsd, aggregate_scores
+from utils import generate_violinplot, generate_stripplot, generate_heatmap, generate_correlation_matrix
 from diffusers import DiffusionPipeline, StableDiffusionPipeline, DPMSolverMultistepScheduler
 from torchmetrics.functional.multimodal import clip_score
 from functools import partial
@@ -27,6 +27,7 @@ if len(os.listdir("numpy")) != 0:
         TENG_scores.append(np.load(f"numpy/{model}TENGscores.npy"))
     GMSD_matrices = np.load(f"numpy/GMSDmatrices.npy")
     prompts = np.load("numpy/prompts.npy")
+
 else:
     CLIP_scores, NIQE_scores, BRISQUE_scores, TENG_scores = [], [], [], []
     for model in model_names:
@@ -36,6 +37,33 @@ else:
         TENG_scores.append(np.array([]))
     GMSD_matrices = np.array([])
     prompts = np.array([])
+
+# Load diffusion models: base models, refiner models, and old models
+models = []
+pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
+models.append(pipe)
+refiner = DiffusionPipeline.from_pretrained(
+    "stabilityai/stable-diffusion-xl-refiner-1.0",
+    text_encoder_2=pipe.text_encoder_2,
+    vae=pipe.vae,
+    torch_dtype=torch.float16,
+    use_safetensors=True,
+    variant="fp16",
+)
+models.append(refiner)
+pipe0_9 = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-0.9", torch_dtype=torch.float16, variant="fp16", use_auth_token=ACCESS_TOKEN)
+models.append(pipe0_9)
+refiner0_9 = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-refiner-0.9", torch_dtype=torch.float16, variant="fp16", use_auth_token=ACCESS_TOKEN) 
+models.append(refiner0_9)
+pipe1_5 = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16)
+models.append(pipe1_5)
+pipe2_1 = StableDiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-2-1", torch_dtype=torch.float16)
+models.append(pipe2_1)
+pipe2_1.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+
+# Enable model CPU offload for all models
+for model in models:
+    model.enable_model_cpu_offload()
 
 for i in range(1):
     # Configure PaLM; generate prompts & image names
@@ -47,34 +75,6 @@ for i in range(1):
     prompt = prompt.result
     imageName = imageName.result
     prompts = np.append(prompts, prompt)
-
-    # Load diffusion models: base models, refiner models, and old models
-    # TODO: Move this out of for loop
-    models = []
-    pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
-    models.append(pipe)
-    refiner = DiffusionPipeline.from_pretrained(
-        "stabilityai/stable-diffusion-xl-refiner-1.0",
-        text_encoder_2=pipe.text_encoder_2,
-        vae=pipe.vae,
-        torch_dtype=torch.float16,
-        use_safetensors=True,
-        variant="fp16",
-    )
-    models.append(refiner)
-    pipe0_9 = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-0.9", torch_dtype=torch.float16, variant="fp16", use_auth_token=ACCESS_TOKEN)
-    models.append(pipe0_9)
-    refiner0_9 = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-refiner-0.9", torch_dtype=torch.float16, variant="fp16", use_auth_token=ACCESS_TOKEN) 
-    models.append(refiner0_9)
-    pipe1_5 = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16)
-    models.append(pipe1_5)
-    pipe2_1 = StableDiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-2-1", torch_dtype=torch.float16)
-    models.append(pipe2_1)
-    pipe2_1.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
-
-    # Enable model CPU offload for all models
-    for model in models:
-        model.enable_model_cpu_offload()
 
     # Generate images for the given prompt
     images = []
@@ -96,8 +96,8 @@ for i in range(1):
     for i in range(len(images)):
         CLIP_scores[i] = np.append(CLIP_scores[i], calculate_clip_score(np.array(images[i]), prompt, clip_score_fn))
         NIQE_scores[i] = np.append(NIQE_scores[i], calculate_niqe(images[i]))
-        BRISQUE_scores[i] = np.append(NIQE_scores[i], calculate_brisque(images[i]))
-        TENG_scores[i] = np.append(NIQE_scores[i], calculate_teng(images[i]))
+        BRISQUE_scores[i] = np.append(BRISQUE_scores[i], calculate_brisque(images[i]))
+        TENG_scores[i] = np.append(TENG_scores[i], calculate_teng(images[i]))
     GMSD_matrices = np.concatenate((GMSD_matrices, [calculate_gmsd(images)]), axis=0) if GMSD_matrices.size else np.array([calculate_gmsd(images)])
 
     # Save scores to externally saved lists
@@ -119,8 +119,7 @@ print(f"Prompts: {prompts}")
 
 # Generate plots
 # TODO: Save plots
-# TODO: Metric Correlation Analysis
-# TODO: Metric Weighting and Aggregation
+metric_scores = np.array([CLIP_scores, NIQE_scores, BRISQUE_scores, TENG_scores])
 generate_violinplot(CLIP_scores, "CLIP")
 generate_stripplot(CLIP_scores, "CLIP")
 generate_violinplot(NIQE_scores, "NIQE")
@@ -130,3 +129,5 @@ generate_stripplot(CLIP_scores, "BRISQUE")
 generate_violinplot(NIQE_scores, "TENG")
 generate_stripplot(NIQE_scores, "TENG")
 generate_heatmap(GMSD_matrices)
+generate_correlation_matrix(metric_scores, model_names)
+aggregate_scores((CLIP_scores, NIQE_scores, BRISQUE_scores, TENG_scores), model_names)
